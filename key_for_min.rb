11841090829233f8448dda588250c0bda20182cd
Ryan Parrish <rino3717@gmail.com>
2020-03-27 01:19:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_key = ""
   low_age = 700
   
-  name_hash.each do |key, age|
+  name_hash.collect do |key, age|
     if age < low_age
       low_age = age
       low_key = key
