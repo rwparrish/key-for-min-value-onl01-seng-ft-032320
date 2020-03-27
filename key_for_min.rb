28db03fe0de67ed7a2bbcd1132ceeb51d96b368b
Ryan Parrish <rino3717@gmail.com>
2020-03-27 01:10:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |key, age|
+  low_key = 
+  low_age = 
+  
+  name_hash.each do |key, age|
+    if age < 
     
+  
+  end
+
 end
